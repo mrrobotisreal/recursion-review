cf18069e96bfeba731eed684df3ba7f59cafde97
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-02-22 15:32:24 -0700

    Finished getElementsByClassName and the spec file for it

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, element
 ) {
-  // your code here
+  // Second parameter (element)
+  console.log('Element: ' + element);
+  // Create accumulator array
+  var accumulator = [];
+  // Set that element to have a value of element if passed or document.body
+  element = element || document.body;
+  // If current element contains the className
+  if (element.classList && element.classList.contains(className)) {
+    // Push to an accumulator array
+    accumulator.push(element);
+  }
+  // If current element has childNodes
+  if (element.hasChildNodes()) {
+    // Iterate through the childNodes
+    element.childNodes.forEach(function(child) {
+      // Recursively call getElementsByClassName on each child node (concat)
+      accumulator = accumulator.concat(getElementsByClassName(className, child));
+    });
+  }
+  // Return accumulator array
+  return accumulator;
 };
